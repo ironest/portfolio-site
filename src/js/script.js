234283bfbd38214b04526675e829737792f73af3
ironest <ironest@gmail.com>
2019-09-22 01:30:12 +1000

    Integration of ScrollReveal JavaScript library

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,46 @@
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  var i;
 
-    //this.classList.toggle("active");
+  for (i = 0; i < coll.length; i++) {
+    console.log("Dentro");
+    coll[i].addEventListener("click", function() {
 
-    postContent = this.parentNode;
+      postContent = this.parentNode;
 
-    postPreview = postContent.getElementsByClassName("postPreview")[0];
-    postLong = postContent.getElementsByClassName("postLong")[0];
+      postPreview = postContent.getElementsByClassName("postPreview")[0];
+      postLong = postContent.getElementsByClassName("postLong")[0];
 
-    if (postLong.style.maxHeight){
-        postLong.style.maxHeight = null;
-        this.innerHTML = "read more...";
-    } else {
-        postLong.style.maxHeight = postLong.scrollHeight + "px";
-        this.innerHTML = "read less...";
-    } 
-  });
-}
+      if (postLong.style.maxHeight){
+          postLong.style.maxHeight = null;
+          this.innerHTML = "read more...";
+      } else {
+          postLong.style.maxHeight = postLong.scrollHeight + "px";
+          this.innerHTML = "read less...";
+      } 
+    });
+  }
+
+ScrollReveal().reveal('.row', {
+    delay: 200,
+    interval: 400,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-out',
+});
+
+ScrollReveal().reveal('.projComponent', {
+    delay: 200,
+    interval: 400,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-out',
+});
+
+ScrollReveal().reveal('.postComponent', {
+    delay: 200,
+    interval: 400,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-out',
+});
